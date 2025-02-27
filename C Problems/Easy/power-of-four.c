1d37659f095ca37b1787482ceaf9b72b06040f74
Tizhqy <444080@ogr.ktu.edu.tr>
2025-02-27 18:38:43 +0300

    Create power-of-four.c

---
+++ C Problems/Easy/power-of-four.c
@@ -0,0 +1,15 @@
+bool isPowerOfFour(int n) {
+    if (n<=0) return 0;
+
+    
+    while (n>1)
+    {
+        if (n%4!=0)
+        {
+            return false;
+        }
+        n/=4;
+    }
+    return true;    
+    
+}
