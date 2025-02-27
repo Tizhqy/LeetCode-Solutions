2883631252b00dece866164592bdd4a593eb2596
Tizhqy <444080@ogr.ktu.edu.tr>
2025-02-27 18:28:17 +0300

    Update sqrtx.c

--- C Problems/Easy/sqrtx.c
+++ C Problems/Easy/sqrtx.c
@@ -1,14 +1,10 @@
-#include <stdio.h>
-
 int mySqrt(int x) {
-    int y;
-    
-    int* result=(int*)malloc(2 * sizeof(int));
-    
+    long long y;
+    long long i;
     if (x==0 || x==1){
         return x;
     }
-    for (int i = 1; i <= x/2; i++){
+    for (i = 1; i <= x/2; i++){
         if (i*i <=x && (i+1)*(i+1)>x){
             y=i;
             break;;
